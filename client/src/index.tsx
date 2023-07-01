769abb688422c201e1647ad2d910c204bc52b18e
vedXpatel <ved.gp@somaiya.edu>
2023-07-01 20:34:02 +0530

    feature-2: adding chakra-ui and color modes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+      <ChakraProvider >
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
-  </React.StrictMode>
+      </ChakraProvider>
 );
