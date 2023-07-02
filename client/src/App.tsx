2fa42fb3e37455432a513aa7f291c0440bee7445
vedXpatel <ved.gp@somaiya.edu>
2023-07-02 13:12:05 +0530

    dev-4: adding login page and ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Options from "./pages/Options";
+import Login from "./pages/Login";
 
 function App() {
   return (
    <Router>
      <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
       <Route path="/options" element={<Options/>} />
      </Routes>
    </Router>
