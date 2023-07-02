4152c4a83db2692042df0d60077d6d321ddb1891
advait khandare <advait.khandare@gmail.com>
2023-07-02 12:29:06 +0530

    filteradded

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Options from "./pages/Options";
+import OptionChainComponent from './filter/Symbolfilter';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/options" element={<Options/>} />
+      <Route path ="/filter" element={<OptionChainComponent/>} />
      </Routes>
    </Router>
   );
