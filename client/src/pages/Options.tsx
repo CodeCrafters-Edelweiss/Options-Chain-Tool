b9367d316e87a289321e2e4e768426b99521b3e3
Abhiraj Kale <abhirajkale1806@gmail.com>
2023-07-04 16:38:08 +0530

    added new navbar

--- client/src/pages/Options.tsx
+++ client/src/pages/Options.tsx
@@ -1,17 +1,33 @@
-import React, {useState,useEffect} from 'react';
-import TableComp from '../components/TableComp';
-import Navbar from '../components/Navbar';
+import React, { useState, useEffect } from "react";
+import TableComp from "../components/TableComp";
+import Navbar from "../components/Navbar";
+import logo from "../assets/icons8-trading-64.png";
 import "../styles/options.css";
+import { Flex, Box } from "@chakra-ui/react";
 
 const Options = () => {
-        return (
-            <div>
-                <Navbar/>
-                <div className="options-container">
-                    <TableComp/>
-                </div>
-            </div>
-        );
+  return (
+    <div>
+      <Flex
+        bg="black"
+        color="yellow"
+        align="center"
+        justify="space-between"
+        p={4}
+      >
+        <Box className="logo-container">
+          <a href="/home"><img src={logo} alt="Logo" width="40" height="40" /></a>
+        </Box>
+        <Box>
+          <Navbar />
+        </Box>
+      </Flex>
+
+      <div className="options-container">
+        <TableComp />
+      </div>
+    </div>
+  );
 };
 
 export default Options;
